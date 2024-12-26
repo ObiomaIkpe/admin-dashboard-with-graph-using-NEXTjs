15f316453f371f399cd6aeb9e7fce04d48d04349
ObiomaIkpe <obiomaikpe1@gmail.com>
2024-12-26 12:43:06 +0100

    making the auth page and main page have different layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,19 +31,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute='class' defaultTheme="light" enableSystem={false}
+        <ThemeProvider 
+        attribute='class' 
+        defaultTheme="light" 
+        enableSystem={true}
         storageKey="dashboard-theme">
-        <Navbar />
-        <div className="flex">
-
-          <div className="hidden md:block h-[100vh] w-[300px]">
-            <Sidebar />
-          </div>
-
-          <div className="p-5 w-full md:max-w-[1140px]">
+          
         {children}
-          </div>
-        </div>
+        
         <Toaster />
         </ThemeProvider>
       </body>
